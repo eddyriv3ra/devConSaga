87212c591fe946c40d1d89954f0564251449f649
unknown <eddy.rivera.echegaray@gmail.com>
2018-08-31 22:36:14 -0300

    PureComponet in group inputs

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -1,37 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classnames from 'classnames';
 
-const SelectListGroup = ({
-  name,
-  value,
-  error,
-  info,
-  onChange,
-  options,
-}) => {
-  const selectOptions = options.map(option => (
-    <option key={option.label} value={option.value}>
-      {option.label}
-    </option>
-  ))
-  return (
-    <div className="form-group">
-      <select
-        className={classnames('form-control form-control-lg', {
-          'is-invalid': error
-        })}
-        name={name}
-        value={value}
-        onChange={onChange}
-      >
-        {selectOptions}
-      </select>
-      {info && <small className='form-text text-muted'>{info}</small>}
-      {error && (
-        <div className="invalid-feedback">{error}</div>
-      )}
-    </div>  
-  )
+class SelectListGroup extends PureComponent {
+  render() {
+    const { name, value, error, info, onChange, options} = this.props;
+    const selectOptions = options.map(option => (
+      <option key={option.label} value={option.value}>
+        {option.label}
+      </option>
+    ))
+    return (
+      <div className="form-group">
+        <select
+          className={classnames('form-control form-control-lg', {
+            'is-invalid': error
+          })}
+          name={name}
+          value={value}
+          onChange={onChange}
+        >
+          {selectOptions}
+        </select>
+        {info && <small className='form-text text-muted'>{info}</small>}
+        {error && (
+          <div className="invalid-feedback">{error}</div>
+        )}
+      </div>  
+    )
+  }
 }
 
 export default SelectListGroup;
